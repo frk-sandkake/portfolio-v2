ce8cb4f0740935780c83c62396e891412f4d0352
Linda Sandaker <frk.sandkake@gmail.com>
2023-06-28 17:46:40 +0200

    remove bgcolor on Box + Container

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom"
-import { Box, AppBar, Toolbar, Typography, Button, Container } from "@mui/material"
+import { Box, AppBar, Toolbar, Button, Container } from "@mui/material"
 import { Link, NavLink } from 'react-router-dom'
 import ImageURL from '../assets/logo-desktop.svg'
 
@@ -10,7 +10,7 @@ export default function Layout() {
     }
 
     return (
-        <Box sx={{ bgcolor: '#004346', height: '100vh' }}  >
+        <Box sx={{ height: '100vh' }}  >
             <AppBar position="static" color="backgroundColor">
                 <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
                     <Link className='site-logo' to='/'>
@@ -30,7 +30,7 @@ export default function Layout() {
                     </nav>
             </Toolbar>
             </AppBar>
-            <Container maxWidth='sm' sx={{ bgcolor: '#004346', color: '#fff', height: '100vh' }}>
+            <Container maxWidth='md' sx={{ height: '100vh' }}>
                 <Outlet />
             </Container>
         </Box>

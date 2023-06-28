0b05f7ae5f77831b4eb87bda764ffd2281a1f5de
Linda Sandaker <frk.sandkake@gmail.com>
2023-06-28 17:48:19 +0200

    Removed Grid, added intro, + MUI icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,60 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom'
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
+import ImageURL from '../assets/unicorn-me-open-smile-200x200.jpg'
+import JavascriptTwoToneIcon from '@mui/icons-material/Javascript';
+import CssTwoToneIcon from '@mui/icons-material/CssTwoTone';
+import PublicTwoToneIcon from '@mui/icons-material/PublicTwoTone';
+import ScienceTwoToneIcon from '@mui/icons-material/ScienceTwoTone';
+import AutoStoriesTwoToneIcon from '@mui/icons-material/AutoStoriesTwoTone';
+import PoolTwoToneIcon from '@mui/icons-material/PoolTwoTone';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import AccessibilityNewTwoToneIcon from '@mui/icons-material/AccessibilityNewTwoTone';
 
-
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.secondary.light,
+export const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: 'transparent',
   ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.dark,
+  padding: theme.spacing(0),
+  margin: theme.spacing(0),
+  color: theme.palette.text.secondary,
 }));
 
 
 export default function Home() {
-
   return (
     <Box>
-        <Grid container sx={{ flexGrow: 1 }}>
-
-        <Grid sx={{ flexGrow: 1 }}>
-            <Typography sx={{ flexGrow: 1 }} variant='h1' color='primary'>
-                Home Page
-            </Typography>
-        </Grid>
-        <Grid item xs={6} md={4}>
-          <Item className='bg-color-blue'>xs=6 md=4</Item>
-        </Grid>
-        <Grid >
-           <Button variant="outlined" color="primary">Login</Button>
-        </Grid>
-      </Grid>
+        <Typography component='h1' variant='h2' color='primary'>
+                Portfolio 2.0
+        </Typography>
+      <article class="intro-content" aria-label="Introduction of website" role="article">
+        <img src={ImageURL} width="200" height="200" alt="Me dressed up as a unicorn"/>
+        <h2 class="subheading">
+            Finding my digital voice
+        </h2>
+        <h3> by Linda Sandaker</h3>
+        <p>
+            jr. FrontEnd Developer
+        </p>
+    </article>
+    <div class="topics-wrapper" aria-label="Icons of Frontend and Topics" role="none">
+        <div class="frontend" aria-label="Dev topics that interest me" role="graphics-object">
+          <JavascriptTwoToneIcon aria-label="JavaScript" />
+          <CssTwoToneIcon aria-label="CSS3" />
+          <GitHubIcon aria-label="GitHub" />
+          <AccessibilityNewTwoToneIcon aria-label="Accessibility on the web" />
+        </div>
+        <div class="topics" aria-label="Topics I'm interested in" role="graphics-object">
+          <PublicTwoToneIcon aria-label="Environmental" />
+          <ScienceTwoToneIcon aria-label="Science" />
+          <AutoStoriesTwoToneIcon aria-label="Stories in all forms" />
+          <PoolTwoToneIcon aria-label="all year swimming" />
+        </div>
+      </div>
+      <Button component={Link} to='/projects' color="primary">Check out my latest projects</Button>
     </Box>
   )
 }
